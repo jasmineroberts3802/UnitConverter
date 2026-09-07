9b30ab0b9fec07a5420401eab1a83021bc91fd26
Jasmine <jasmineroberts3802@gmail.com>
2026-09-06 22:31:46 -0400

    Attempt to implement miles to kilometers conversion page

---
+++ src/UnitConverter/Pages/ConversionsModel.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UnitConverter.Pages;
+
+public class ConversionsModel : PageModel
+{
+    public string Input { get; set; } = string.Empty;
+
+    public string Output { get; set; } = string.Empty;
+
+    public void OnGet()
+    {
+        Input = "3.1415";
+
+        ViewData.Add("ConversionType", "Miles to Kilometers");
+
+        ViewData["PageTitle"] = "Conversions";
+
+        Output = new UnitOf.Length().FromMiles(Convert.ToDouble(Input)).ToKilometers().ToString();
+
+    }
+}
