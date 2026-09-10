fb9e3a70cd2850e98086232800ec799c647b7a1f
Jasmine E. Roberts <jrober36@wvup.edu>
2026-09-10 16:01:10 -0400

    Lesson 4: v 1.2

--- src/UnitConverter/Pages/Conversions.cshtml.cs
+++ src/UnitConverter/Pages/Conversions.cshtml.cs
@@ -10,19 +10,72 @@ public class ConversionsModel : PageModel
 
     public string Output { get; set; } = string.Empty;
 
+    // created ConversionType string
     public string ConversionType { get; set; } = string.Empty;
 
-    public void OnGet()
+    // added parameters for input and conversiontype for route binding
+    public void OnGet(string input, string conversionType) // flip parameters
     {
-        Input = "[BindProperty(SupportsGet = true)]";
+        double value;
+        // route bound input parameter
+        Input = input;
 
-        ConversionType = "[BindProperty(SupportsGet = true)]";
+        // route bound conversiontype parameter
+        ConversionType = conversionType;
 
-        ViewData["ConversionType"] = "Miles to Kilometers";
+        // updated view data to display the conversiontype parameter instead of hard coded miles to kilometers
+        ViewData["ConversionType"] = conversionType;
 
         ViewData["Title"] = "Conversions";
 
+        try
+        {
+            value = Convert.ToDouble(input);
+        }
+        catch (Exception /* either InvalidCast, Format, or Argument exception? */ )
+        {
+            // Put the error in ViewData
+
+            // Then
+            return;
+        }
+
+        switch (conversionType.ToLower())
+        {
+            case "milestokilometers":
+                Output = new UnitOf.Length().FromMiles(value).ToKilometers().ToString();
+                break;
+            case "kilometers to miles":
+                // convert kilometers to miles
+                break;
+            case "fahrenheit to celsius":
+                // convert fahrenheit to celsius
+                break;
+            case "celsius to fahrenheit":
+                // convert celsius to fahrenheit
+                break;
+            case "pounds to kilograms":
+                // convert pounds to kilograms
+                break;
+            case "kilograms to pounds":
+                // convert kilograms to pounds
+                break;
+            case /* conversion supported by UnitOf */ :
+                // unitof conversion operation
+                break;
+            case /* reverse conversion supported by UnitOf */:
+                // unitof conversion operation
+                break;
+            default:
+                // viewdata error message
+                break;
+        }
+
+        Output.ToString();
+
         Output = new UnitOf.Length().FromMiles(Convert.ToDouble(Input)).ToKilometers().ToString();
+
+
 
     }
 }
